ad0d2d5859f222378e17df383c1369b3e03b6a82
Soichiro Hirata <hirata_soichiro@cyberagent.co.jp>
2026-01-30 07:34:15 +0900

    connect frontend and api

---
+++ frontend/app/articles/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import ArticleEditForm from '@/components/ArticleEditForm'
+
+export default function ArticleEditPage() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <ArticleEditForm />
+        </div>
+    )
+}
