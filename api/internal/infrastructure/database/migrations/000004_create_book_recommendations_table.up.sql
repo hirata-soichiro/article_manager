e2dde499138b0c1c06b64be4533e55f467f73aad
Soichiro Hirata <hirata_soichiro@cyberagent.co.jp>
2026-02-11 16:36:51 +0900

    fix

--- api/internal/infrastructure/database/migrations/000004_create_book_recommendations_table.up.sql
+++ api/internal/infrastructure/database/migrations/000004_create_book_recommendations_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS book_recommendations (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    recommendations_json TEST NOT NULL,
+    recommendations_json TEXT NOT NULL,
     created_at DATETIME(6) NULL,
     expires_at DATETIME(6) NULL,
     INDEX idx_expires_at (expires_at)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utfmb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
